12efe8edb2b9c25be74590a39b63f33222cb3d0f
Sarovar Shaikh <sarwarshaikh3622@gmail.com>
2022-06-30 02:23:58 +0530

    add operatos

---
+++ findFirstDigit.js
@@ -0,0 +1,5 @@
+// You are provided a four digit number N only. Your task is to print out the first digit of that number.
+const First_Digit = (n) => {
+    const sarovarShaikh = String(n)[0];
+   return(sarovarShaikh);
+ };
---
+++ findLastDigit.js
@@ -0,0 +1,5 @@
+// You are provided a four digit number N only. Your task is to print out the last digit of that number.
+const Last_Digit = (n) => {
+    const sarovarShaaikh = String(n)[3];
+    return (sarovarShaaikh)
+ };
